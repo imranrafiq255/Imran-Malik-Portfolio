e6f910f1a9a660c377f726a33a93202a62626ff1
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-09 01:09:25 +0500

    nothing

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const port = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 app.post("/send-email", (req, res) => {
   const { name, email, message } = req.body;
