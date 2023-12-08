629b0420b0893e0fa4f803001d472a09ea84fb21
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-09 02:21:19 +0500

    vercel changes

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const port = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../server/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../server/client/build/index.html"));
 });
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Imran Malik" });
